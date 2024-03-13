31369dd3eed1382b7e9ea5116febe95ff95088d0
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-03-13 22:06:59 +0200

    cors error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("upload"));
 const cors = require("cors");
 // CORS middleware with specific origin
 app.use(cors({
-    origin: 'elfarama.com', // Replace with your frontend's domain
+    origin: 'https://elfarama.com', // Replace with your frontend's domain
     methods: ['GET', 'POST','DELETE','PUT'], // Allow only specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow only specific headers
   }));
