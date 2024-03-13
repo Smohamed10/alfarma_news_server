3c4b5d3535de3733e4924062659f75a49b591087
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-03-13 22:29:01 +0200

    BODY NOT PARAMS

--- routes/news/getnew.js
+++ routes/news/getnew.js
@@ -10,11 +10,12 @@ const authorized = require("../../middleware/authorized");
 //======= function increment the rate of post when get the post =====//
 router.get("/:id",async(req,res)=>{
     const query = util.promisify(connection.query).bind(connection); // transform query to promise to can use await/ async
-    const newspost=  await query ("select * from news where id = ?",req.params.id);
-    await query ("UPDATE news SET seen = seen + 1 WHERE id = ?",req.params.id);
+    const newspost=  await query ("select * from news where id = ?",req.body.id);
+    await query ("UPDATE news SET seen = seen + 1 WHERE id = ?",req.body.id);
     res.status(202).json(newspost);
     
 })
+
 
 
 module.exports = router;
