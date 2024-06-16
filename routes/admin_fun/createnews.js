d871d4acfbb5cc73565055c25656dd91c4650be1
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-06-16 15:47:12 +0300

    old create news

--- routes/admin_fun/createnews.js
+++ routes/admin_fun/createnews.js
@@ -1,28 +1,18 @@
 const router = require("express").Router();
 const connection = require("../../db/dbconnection.js");
-const util = require("util");
+const { body, validationResult } = require("express-validator");
+const util = require("util"); // helper
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const admin = require("../../middleware/admin");
+// function create newss
 
-// Endpoint for handling image uploads
-router.post("/upload", admin, async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).send("No file uploaded.");
-    }
-    // Save the file path or URL to the database and return the URL
-    const filePath = req.file.path; // Adjust this based on your server setup
-    res.status(200).json({ url: `http://api.elfarama.com.com/${filePath}` }); // Adjust the URL
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Endpoint for creating news
 router.post("/", admin, async (req, res) => {
+  // get input
   const { writer_name, name, content, category, pic_path, publicID } = req.body;
   try {
-    const query = util.promisify(connection.query).bind(connection);
+    const query = util.promisify(connection.query).bind(connection); // transform query to promise to can use await/ async
+    // get the current time
     function getCurrentDateInCairo() {
       const options = {
         timeZone: "Africa/Cairo",
@@ -34,27 +24,25 @@ router.post("/", admin, async (req, res) => {
         second: "numeric",
         hour12: true,
       };
+
       return new Date().toLocaleString("en-US", options);
     }
     const time2 = getCurrentDateInCairo();
 
-    // Modify the news object to include the image URL
     const newsobj = {
-      writer_name,
-      name,
-      content,
-      category,
-      pic_path, // Use the image URL received from the frontend
+      writer_name: writer_name,
+      name: name,
+      content: content,
+      category: category,
+      pic_path: pic_path,
       time: time2,
-      publicID,
+      publicID: publicID,
     };
-
-    // Insert the object into the database
-    await query("INSERT INTO news SET ?", newsobj);
-    res.status(200).json(newsobj);
+    // insert the object in data base
+    await query("insert into news set ? ", newsobj),
+      res.status(200).json(newsobj);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(404).json(err);
   }
 });
 
